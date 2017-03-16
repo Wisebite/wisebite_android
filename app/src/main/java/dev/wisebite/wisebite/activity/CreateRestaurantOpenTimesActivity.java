7ea54484cf9bfaad4520f476f3de659079ba3bed
albert <albert.suarez.molgo@est.fib.upc.edu>
2017-03-16 16:57:56 +0100

    design open times activity and interaction with time pickers

--- app/src/main/java/dev/wisebite/wisebite/activity/CreateRestaurantOpenTimesActivity.java
+++ app/src/main/java/dev/wisebite/wisebite/activity/CreateRestaurantOpenTimesActivity.java
@@ -1,20 +1,27 @@
 package dev.wisebite.wisebite.activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import dev.wisebite.wisebite.R;
 import dev.wisebite.wisebite.domain.Restaurant;
+import dev.wisebite.wisebite.utils.Utils;
 
 public class CreateRestaurantOpenTimesActivity extends AppCompatActivity {
 
     public static final String RESTAURANT = "INTENT_RESTAURANT";
 
     private Restaurant restaurant;
+    private LayoutInflater inflater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,8 @@ public class CreateRestaurantOpenTimesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_restaurant_open_times);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        inflater = LayoutInflater.from(CreateRestaurantOpenTimesActivity.this);
 
         if (getIntent().getSerializableExtra(RESTAURANT) != null) {
             this.restaurant = (Restaurant) getIntent().getSerializableExtra(RESTAURANT);
@@ -37,4 +46,45 @@ public class CreateRestaurantOpenTimesActivity extends AppCompatActivity {
         });
     }
 
+    public void showTimePicker(final View view) {
+        final TimePicker firstTimePicker = (TimePicker) inflater.inflate(getResources().getLayout(R.layout.time_picker), null);
+        firstTimePicker.setIs24HourView(true);
+        new AlertDialog.Builder(CreateRestaurantOpenTimesActivity.this)
+                .setTitle(getResources().getString(R.string.title_first_time_picker))
+                .setMessage(getResources().getString(R.string.message_first_time_picker))
+                .setView(firstTimePicker)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        final TimePicker secondTimePicker = (TimePicker) inflater.inflate(getResources().getLayout(R.layout.time_picker), null);
+                        secondTimePicker.setIs24HourView(true);
+                        new AlertDialog.Builder(CreateRestaurantOpenTimesActivity.this)
+                                .setTitle(getResources().getString(R.string.title_second_time_picker))
+                                .setMessage(getResources().getString(R.string.message_second_time_picker))
+                                .setView(secondTimePicker)
+                                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        TextView textView = (TextView) view;
+                                        textView.setText(Utils.parseStartEndDate(firstTimePicker, secondTimePicker));
+                                    }
+                                })
+                                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        // do nothing
+                                    }
+                                })
+                                .show();
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .show();
+
+    }
 }
--- app/src/main/java/dev/wisebite/wisebite/utils/Utils.java
+++ app/src/main/java/dev/wisebite/wisebite/utils/Utils.java
@@ -1,5 +1,9 @@
 package dev.wisebite.wisebite.utils;
 
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.widget.TimePicker;
+
 import com.firebase.client.Firebase;
 
 /**
@@ -12,4 +16,27 @@ public class Utils {
         return new Firebase("https://wisebite-f7a53.firebaseio.com/").push().getKey();
     }
 
+    @TargetApi(Build.VERSION_CODES.M)
+    public static String parseStartEndDate(TimePicker firstTimePicker, TimePicker secondTimePicker) {
+        int firstHour, firstMinute, secondHour, secondMinute;
+        firstHour = firstTimePicker.getHour();
+        firstMinute = firstTimePicker.getMinute();
+        secondHour = secondTimePicker.getHour();
+        secondMinute = secondTimePicker.getMinute();
+
+        String parsed = "";
+        if (firstHour < 10) parsed += '0';
+        parsed += String.valueOf(firstHour) + ':';
+        if (firstMinute < 10) parsed += '0';
+        parsed += String.valueOf(firstMinute);
+
+        parsed += " - ";
+
+        if (secondHour < 10) parsed += '0';
+        parsed += String.valueOf(secondHour) + ':';
+        if (secondMinute < 10) parsed += '0';
+        parsed += String.valueOf(secondMinute);
+
+        return parsed;
+    }
 }
