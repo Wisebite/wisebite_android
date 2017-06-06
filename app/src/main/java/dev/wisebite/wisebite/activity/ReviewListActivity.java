204a22af45643ebbab87d922bdd753e0103d6fa0
albert <albert.suarez.molgo@est.fib.upc.edu>
2017-06-06 16:55:07 +0200

    parse objectId to list of reviews

--- app/src/main/java/dev/wisebite/wisebite/activity/ReviewListActivity.java
+++ app/src/main/java/dev/wisebite/wisebite/activity/ReviewListActivity.java
@@ -7,10 +7,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import dev.wisebite.wisebite.R;
+import dev.wisebite.wisebite.domain.Dish;
+import dev.wisebite.wisebite.domain.Menu;
+import dev.wisebite.wisebite.domain.Restaurant;
+import dev.wisebite.wisebite.domain.Review;
 import dev.wisebite.wisebite.service.DishService;
 import dev.wisebite.wisebite.service.MenuService;
 import dev.wisebite.wisebite.service.RestaurantService;
+import dev.wisebite.wisebite.service.ReviewService;
 import dev.wisebite.wisebite.service.ServiceFactory;
 
 public class ReviewListActivity extends AppCompatActivity {
@@ -21,6 +30,9 @@ public class ReviewListActivity extends AppCompatActivity {
     private RestaurantService restaurantService;
     private DishService dishService;
     private MenuService menuService;
+    private ReviewService reviewService;
+
+    private List<Review> reviews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +45,34 @@ public class ReviewListActivity extends AppCompatActivity {
         restaurantService = ServiceFactory.getRestaurantService(ReviewListActivity.this);
         dishService = ServiceFactory.getDishService(ReviewListActivity.this);
         menuService = ServiceFactory.getMenuService(ReviewListActivity.this);
+        reviewService = ServiceFactory.getReviewService(ReviewListActivity.this);
         if (getIntent().getSerializableExtra(DISH_ID) != null) {
             this.id = getIntent().getExtras().getString(DISH_ID);
         }
 
+        this.reviews = getReviews();
+
+    }
+
+    private List<Review> getReviews() {
+        List<Review> reviewList = new ArrayList<>();
+
+        Restaurant restaurant = restaurantService.get(id);
+        Dish dish = dishService.get(id);
+        Menu menu = menuService.get(id);
+
+        List<String> reviewKeys = new ArrayList<>();
+        if (restaurant != null && restaurant.getReviews() != null) reviewKeys.addAll(restaurant.getReviews().keySet());
+        else if (dish != null && dish.getReviews() != null) reviewKeys.addAll(dish.getReviews().keySet());
+        else if (menu != null && menu.getReviews() != null) reviewKeys.addAll(menu.getReviews().keySet());
+
+        Review review;
+        for (String key : reviewKeys) {
+            review = reviewService.get(key);
+            if (review != null) reviewList.add(review);
+        }
+
+        return reviewList;
     }
 
 }
